8b1d96a987a5d8f189f1e8caa5863e64ffb3118f
戈登走過去 <gordonwalkedby@gmail.com>
2021-03-09 20:51:08 +0800

    ByScript

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,20 @@
 Set-Location $PSScriptRoot
 Set-Location "./base_vb/"
 dotnet run g
-git add -A 
-git commit -m "ByScript"
-git push gitee master -f 
-git push github master -f 
-git push gitlab master -f 
+Set-Location $PSScriptRoot    
+$xml = Get-Item -Path "./docs/atom.xml"
+if ($xml.Exists -and $xml.Length -gt 2000 ) {
+    git add -A 
+    git commit -m "ByScript"
+    Write-Host "开始 push 到 gitee"
+    git push gitee master 
+    Write-Host "开始 push 到 github"
+    git push github master 
+    Write-Host "开始 push 到 gitlab"
+    git push gitlab master 
+    Write-Host " push 完成"
+}
+else {
+    Write-Error "生成信息很可能有误！请检查！！"
+}
 Set-Location $PSScriptRoot
