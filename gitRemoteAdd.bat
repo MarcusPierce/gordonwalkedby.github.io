627864c5d5a91ccdb74c8379c4b8076449ca12e4
wby <gordonwalkedby@gmail.com>
2021-04-14 20:40:03 +0800

    ByScript

---
+++ gitRemoteAdd.bat
@@ -0,0 +1,4 @@
+git remote add gitee git@gitee.com:walkedby/walkedby.git
+git remote add github git@github.com:gordonwalkedby/gordonwalkedby.github.io.git
+git remote add gitlab git@gitlab.com:gordonwalkedby/gordonwalkedby.gitlab.io.git
+git remote rm origin
