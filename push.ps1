6515a767a7ff3594e694323abfaf8e592aaf0f7c
gordonwalkedby <gordonwalkedby@gmail.com>
2021-10-02 09:21:16 +0800

    ByScript

--- push.ps1
+++ push.ps1
@@ -1,7 +1,11 @@
 ﻿Set-Location $PSScriptRoot
+$docs = "./docs/"
+if (Test-Path -Path $docs) {
+    Remove-Item -Path $docs -Recurse -Force
+}
 dotnet "./generator/bin/Debug/netcoreapp3.1/generator.dll" g
 Set-Location $PSScriptRoot
-$xml = Get-Item -Path "./docs/atom.xml"
+$xml = Get-Item -Path ($docs + "atom.xml")
 if ($xml.Exists -and $xml.Length -gt 2000 ) {
     git add -A 
     git commit -m "ByScript" -S
